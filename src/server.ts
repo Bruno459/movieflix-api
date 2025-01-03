3293ca64cc0d406b6cafd0f53b9a4007cbab7541
Bruno459 <bruno45834@gmail.com>
2025-01-02 22:25:24 -0300

    adicionad remoção de filmes

--- src/server.ts
+++ src/server.ts
@@ -82,6 +82,24 @@ app.put('/movies/:id', async (req, res) => {
     }
 });
 
+app.delete('/movies/:id', async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+    const movie = await prisma.movie.findUnique({ where: { id }})
+
+    if(!movie) {
+        return res.status(404).send( { message: "O filme não foi encontrado" } )
+    }
+
+    await prisma.movie.delete( { where: { id } } );
+    }catch(err) {
+        return res.status(500).send( { message: "Não foi possível remover o filme" } )
+    }
+
+    res.status(200).send();
+});
+
 app.listen(port, () => {
     console.log(`servidor está rodando em http://localhost:${port}/`);
 })
